4de1d7a0c2688f8c05c9d8fcb3d2b047a0bffa0e
Mike Jeffcott <mjeffcott@zendesk.com>
2017-06-19 16:42:18 +1000

    Add initial documentation around FeatureHelper module

--- lib/support/helpers/feature_helper.rb
+++ lib/support/helpers/feature_helper.rb
@@ -1,4 +1,24 @@
 module FeatureHelper
+  # Filters list of defined endpoints down to endpoints applicable to the current spec.
+  #
+  # Assuming a targets file like the one in the example below, this method will return an array
+  # containing the objects (as hashes) from the +endpoints+ array where the +features+ array
+  # contains a string that fuzzy matches on the current spec filename.
+  #
+  # If the +features+ array is not present for a given endpoint, it will just run by default.
+  # @example Example targets JSON file
+  #   {
+  #     "base_url": "https://example.com",
+  #     "endpoints": [
+  #       {
+  #         "url": "/users/1",
+  #         "features": [
+  #           { "name": "clickjacking_defense" }
+  #         ]
+  #       }
+  #     ]
+  #   }
+  # @return [Array<Hash{String => String}>]
   def applicable_endpoints
     current_targets.endpoints.select do |endpoint|
       if endpoint.features && !endpoint.features.empty?
@@ -7,7 +27,7 @@ module FeatureHelper
           should_run_for_endpoint?(feature.name)
         end
       else
-        # Run for the current endpoint because no features list exists to filter on
+        # Run for this endpoint because no features list exists to filter on
         true
       end
     end
@@ -15,9 +35,13 @@ module FeatureHelper
 
   private
 
+  # Checks in the call stack for calls originating from a spec file with a filename
+  # that matches +feature+. Returns true if a matching call is found.
+  #
+  # @api private
+  # @param feature [String] Name of feature, read from targets file
+  # @return [Boolean]
   def should_run_for_endpoint?(feature)
-    # Check for spec that matches feature name in call stack to determine if the current spec
-    # should be run against the current endpoint
     caller.any? do |line|
       line.include?(feature)
     end
